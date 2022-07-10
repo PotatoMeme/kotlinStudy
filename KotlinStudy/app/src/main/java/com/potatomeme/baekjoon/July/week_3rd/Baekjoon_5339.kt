4ceea9e855876453437cdc5859e6652e100f1b51
PotatoMeme <kshk0897@gmail.com>
2022-07-10 21:55:39 +0900

    백준 5339

---
+++ KotlinStudy/app/src/main/java/com/potatomeme/baekjoon/July/week_3rd/Baekjoon_5339.kt
@@ -0,0 +1,17 @@
+package com.potatomeme.baekjoon.July.week_3rd
+
+
+fun main() = with(System.out.bufferedWriter()) {
+    write("     /~\\\n" +
+            "    ( oo|\n" +
+            "    _\\=/_\n" +
+            "   /  _  \\\n" +
+            "  //|/.\\|\\\\\n" +
+            " ||  \\ /  ||\n" +
+            "============\n" +
+            "|          |\n" +
+            "|          |\n" +
+            "|          |")
+    close()
+}
+
