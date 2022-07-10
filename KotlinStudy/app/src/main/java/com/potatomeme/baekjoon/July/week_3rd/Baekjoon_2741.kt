bca3a8b6e970465818324f8c97940b0972dc97eb
PotatoMeme <kshk0897@gmail.com>
2022-07-10 20:28:21 +0900

    백준 2741

---
+++ KotlinStudy/app/src/main/java/com/potatomeme/baekjoon/July/week_3rd/Baekjoon_2741.kt
@@ -0,0 +1,10 @@
+package com.potatomeme.baekjoon.July.week_3rd
+
+fun main() = with(System.out.bufferedWriter()) {
+    val br = System.`in`.bufferedReader()
+    for (i in 1..br.readLine().toInt()) {
+        write("${i}\n")
+    }
+    close()
+}
+
