8d2cbb31433df4cdebb8a1a13e548e039a403034
PotatoMeme <kshk0897@gmail.com>
2022-09-30 14:52:57 +0900

    ModernAndroid : BookSearch, Fragment&ViewBinding setting

---
+++ ModernAndroid/booksearch/src/main/java/com/potatomeme/booksearch/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.potatomeme.booksearch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.potatomeme.booksearch.databinding.ActivityMainBinding
+import com.potatomeme.booksearch.ui.view.FavoriteFragment
+import com.potatomeme.booksearch.ui.view.SearchFragment
+import com.potatomeme.booksearch.ui.view.SettingsFragment
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingsFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
