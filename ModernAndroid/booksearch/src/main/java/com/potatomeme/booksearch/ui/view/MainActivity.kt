75807209ea7eed092b22da5b83997082bf22e3ec
PotatoMeme <kshk0897@gmail.com>
2022-10-09 18:34:58 +0900

    ModernAndroid : BookSearch Navigation

--- ModernAndroid/booksearch/src/main/java/com/potatomeme/booksearch/ui/view/MainActivity.kt
+++ ModernAndroid/booksearch/src/main/java/com/potatomeme/booksearch/ui/view/MainActivity.kt
@@ -3,6 +3,12 @@ package com.potatomeme.booksearch.ui.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.potatomeme.booksearch.R
 import com.potatomeme.booksearch.data.repository.BookSearchRepositoryImpl
 import com.potatomeme.booksearch.databinding.ActivityMainBinding
@@ -15,44 +21,67 @@ class MainActivity : AppCompatActivity() {
     }
 
     lateinit var bookSearchViewModel: BookSearchViewModel
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        setupBottomNavigationView()
-        if (savedInstanceState == null) {
-            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
-        }
+//        setupBottomNavigationView()
+//        if (savedInstanceState == null) {
+//            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+//        }
+
+        setupJetpackNavigation()
 
         val bookSearchRepository = BookSearchRepositoryImpl()
         val factory = BookSearchViewModelProviderFactory(bookSearchRepository, this)
         bookSearchViewModel = ViewModelProvider(this, factory)[BookSearchViewModel::class.java]
     }
 
-    private fun setupBottomNavigationView() {
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.fragment_search -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SearchFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_favorite -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, FavoriteFragment())
-                        .commit()
-                    true
-                }
-                R.id.fragment_settings -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.frame_layout, SettingsFragment())
-                        .commit()
-                    true
-                }
-                else -> false
-            }
-        }
+    private fun setupJetpackNavigation() {
+        val host = supportFragmentManager
+            .findFragmentById(R.id.booksearch_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(
+            //navController.graph
+            setOf(
+                R.id.fragment_search, R.id.fragment_favorite, R.id.fragment_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+//    private fun setupBottomNavigationView() {
+//        binding.bottomNavigationView.setOnItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.fragment_search -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SearchFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_favorite -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, FavoriteFragment())
+//                        .commit()
+//                    true
+//                }
+//                R.id.fragment_settings -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.frame_layout, SettingsFragment())
+//                        .commit()
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
 }
