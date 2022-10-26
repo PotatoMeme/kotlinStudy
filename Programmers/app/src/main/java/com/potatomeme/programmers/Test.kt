8bd0416b02b7bd89e2e06b2ece432591ca76b3bd
PotatoMeme <kshk0897@gmail.com>
2022-10-26 20:15:47 +0900

    Programmers : solve 12951

--- Programmers/app/src/main/java/com/potatomeme/programmers/Test.kt
+++ Programmers/app/src/main/java/com/potatomeme/programmers/Test.kt
@@ -1,13 +1,7 @@
 package com.potatomeme.programmers
 
 
-
 fun main() {
-    val s = "-1 -2 -3 -4"
-    var list = mutableListOf<Int>()
-    var save = ""
-    s.split(" ").forEach {
-        list.add(it.toInt())
-    }
-   print(list)
+
+
 }
