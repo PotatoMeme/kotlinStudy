0eef2f77b67f3ab3e4f4aa1192cd02f15c98016a
PotatoMeme <kshk0897@gmail.com>
2022-10-26 18:36:58 +0900

    Programmers : solve 12932

---
+++ Programmers/app/src/main/java/com/potatomeme/programmers/codingtest/years22/october/level1/12932.kt
@@ -0,0 +1,34 @@
+package com.potatomeme.programmers.codingtest.years22.october.level1
+
+// 자연수 뒤집어 배열로 만들기
+// 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=kotlin
+
+// 문제 설명
+//자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+//
+//제한 조건
+//n은 10,000,000,000이하인 자연수입니다.
+//입출력 예
+//n	return
+//12345	[5,4,3,2,1]
+
+class Solution12932 {
+    fun mySolution1(n: Long): IntArray {
+        var save = n
+        var answer = intArrayOf()
+        while (save >= 1) {
+            answer += (save % 10).toInt()
+            save /= 10
+        }
+        return answer
+    }
+
+
+    fun userSolution1(n: Long): IntArray =
+        n.toString().reversed().map {
+            it.toString().toInt()
+        }.toIntArray()
+
+}
+
+
