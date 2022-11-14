d18350535d84cc789b25239c0ea758737c0801b3
PotatoMeme <kshk0897@gmail.com>
2022-11-14 11:05:53 +0900

    Programmers : solve 120816

---
+++ Programmers/app/src/main/java/com/potatomeme/programmers/codingtest/years22/november/level0/120816.kt
@@ -0,0 +1,37 @@
+package com.potatomeme.programmers.codingtest.years22.november.level0
+
+//logo
+//코딩테스트 연습
+//코딩테스트 입문
+//피자 나눠 먹기 (3)
+//도움말
+//컴파일 옵션
+//피자 나눠 먹기 (3)
+//문제 설명
+//머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+//
+//제한사항
+//2 ≤ slice ≤ 10
+//1 ≤ n ≤ 100
+//입출력 예
+//slice	n	result
+//7	10	2
+//4	12	3
+//입출력 예 설명
+//입출력 예 #1
+//
+//10명이 7조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 2판을 시켜야 합니다.
+//입출력 예 #2
+//
+//12명이 4조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 3판을 시켜야 합니다.
+import kotlin.math.*
+class Solution120816 {
+    fun solution_try1(slice: Int, n: Int): Int {
+        return ceil(n.toDouble()/slice).toInt()
+    }//0.2ms
+
+    fun solution_try2(slice: Int, n: Int): Int {
+        val x = n/slice
+        return if (x*slice ==n) return x else x+1
+    }//0.01~0.02ms
+}
