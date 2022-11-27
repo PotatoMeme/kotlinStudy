f5ce9c3d074ef4a0bec2feb0583c0a30ac1478b9
PotatoMeme <kshk0897@gmail.com>
2022-11-27 23:06:47 +0900

    Programmers : solve 120897

---
+++ Programmers/app/src/main/java/com/potatomeme/programmers/codingtest/years22/november/level0/120897.kt
@@ -0,0 +1,50 @@
+package com.potatomeme.programmers.codingtest.years22.november.level0
+
+import kotlin.math.sqrt
+
+//약수 구하기
+//문제 설명
+//정수 n이 매개변수로 주어질 때, n의 약수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//1 ≤ n ≤ 10,000
+//입출력 예
+//n	result
+//24	[1, 2, 3, 4, 6, 8, 12, 24]
+//29	[1, 29]
+//입출력 예 설명
+//입출력 예 #1
+//
+//24의 약수를 오름차순으로 담은 배열 [1, 2, 3, 4, 6, 8, 12, 24]를 return합니다.
+//입출력 예 #2
+//
+//29의 약수를 오름차순으로 담은 배열 [1, 29]를 return합니다.
+
+class Solution120897 {
+    fun solution(n: Int): List<Int> {
+        var answer = mutableListOf<Int>()
+
+        for (i in 1 until sqrt(n.toDouble()).toInt() + 1) {
+            if (n % i == 0) {
+                answer.add(i)
+                if (i * i != n) {
+                    answer.add(n / i)
+                }
+            }
+        }
+
+        return answer.sorted()
+    }//5.98 ~ 21.62ms
+
+    fun solution_try(n: Int): List<Int> {
+        var answer = mutableListOf<Int>()
+
+        for (i in 1..n) {
+            if (n % i == 0) {
+                answer.add(i)
+            }
+        }
+
+        return answer
+    }//0.02 ~ 0.29ms
+}
