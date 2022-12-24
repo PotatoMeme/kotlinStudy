cb6db46437770efedfceafe0cf48fa0f1487f539
PotatoMeme <kshk0897@gmail.com>
2022-12-24 17:01:11 +0900

    [programmers] - [solve] 42576

---
+++ Programmers/app/src/main/java/com/potatomeme/programmers/codingtest/years22/december/level1/42576.kt
@@ -0,0 +1,72 @@
+package com.potatomeme.programmers.codingtest.years22.december.level1
+
+//완주하지 못한 선수
+//문제 설명
+//수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+//
+//마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+//
+//제한사항
+//마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+//completion의 길이는 participant의 길이보다 1 작습니다.
+//참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+//참가자 중에는 동명이인이 있을 수 있습니다.
+//입출력 예
+//participant	completion	return
+//["leo", "kiki", "eden"]	["eden", "kiki"]	"leo"
+//["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
+//["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
+//입출력 예 설명
+//예제 #1
+//"leo"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+//
+//예제 #2
+//"vinko"는 참여자 명단에는 있지만, 완주자 명단에는 없기 때문에 완주하지 못했습니다.
+//
+//예제 #3
+//"mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다
+
+
+class Solution42576 {
+    // 내마음대로 풀기
+    fun solution(
+        participant: Array<String>, completion: Array<String>,
+    ): String {
+        val sorted_participant = participant.sorted()
+        val sorted_completion = participant.sorted()
+        for (i in sorted_completion.indices) {
+            if (sorted_participant[i] != sorted_completion[i]) return sorted_participant[i]
+        }
+        return sorted_participant.last()
+    }
+
+    // 해시로 풀어보자
+    fun solutionToHash(
+        participant: Array<String>, completion: Array<String>,
+    ): String {
+        var map = participant.associateWith { 0 }.toMutableMap()
+        participant.forEach { map[it] = map[it]!! + 1 }
+        completion.forEach { map[it] = map[it]!! - 1 }
+        map.forEach { if (it.value == 1) return it.key }
+        return ""
+    }
+}
+
+//Java,이전에 Java로 푼것
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//class Solution {
+//    public String solution(String[] participant, String[] completion) {
+//        ArrayList<String> part = new ArrayList<String>(Arrays.asList(participant));
+//        ArrayList<String> comp = new ArrayList<String>(Arrays.asList(completion));
+//        Collections.sort(part);
+//        Collections.sort(comp);
+//        for (int i = 0; i < part.size() - 1; i++) {
+//            if (!(part.get(i).equals(comp.get(i)))) {
+//                return part.get(i);
+//            }
+//        }
+//        return part.get(part.size() - 1);
+//    }
+//}
