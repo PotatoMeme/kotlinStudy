e2e85331130ba200bc6818e68dc75d79373bead5
PotatoMeme <kshk0897@gmail.com>
2022-12-28 11:41:05 +0900

    [programmers] - [solve] 12909

---
+++ Programmers/app/src/main/java/com/potatomeme/programmers/codingtest/years22/december/level2/12909.kt
@@ -0,0 +1,63 @@
+package com.potatomeme.programmers.codingtest.years22.december.level2
+
+import java.util.*
+
+//올바른 괄호
+//문제 설명
+//괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+//
+//"()()" 또는 "(())()" 는 올바른 괄호입니다.
+//")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+//'(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+//
+//제한사항
+//문자열 s의 길이 : 100,000 이하의 자연수
+//문자열 s는 '(' 또는 ')' 로만 이루어져 있습니다.
+//입출력 예
+//s	answer
+//"()()"	true
+//"(())()"	true
+//")()("	false
+//"(()("	false
+//입출력 예 설명
+//입출력 예 #1,2,3,4
+//문제의 예시와 같습니다.
+
+//#1
+//)전은 (여야한다
+//이렇게 제외를 다하였을때
+
+
+
+class Solution12909 {
+
+    // 내마음대로 풀기
+    private val c = '('
+    fun solution(s: String): Boolean {
+        var stack = Stack<Char>()
+        s.forEach {
+            if (it == c){
+                stack.add(it)
+            }else if (stack.isEmpty()){
+                return false
+            }else{
+                stack.pop()
+            }
+        }
+        return stack.isEmpty()
+    }
+
+    fun solution_refact(s: String): Boolean {
+        var count = 0
+        for (element in s) {
+            if (element == c) {
+                count++
+            } else if (count == 0) {
+                return false
+            } else {
+                count--
+            }
+        }
+        return count == 0
+    }
+}
