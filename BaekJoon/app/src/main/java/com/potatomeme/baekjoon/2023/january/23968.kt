6ce5ce31ceef33ca5db836bd0cb6825936c9ce4c
PotatoMeme <kshk0897@gmail.com>
2023-01-10 11:11:05 +0900

    [baekjoon] - [solve] 23968

---
+++ BaekJoon/app/src/main/java/com/potatomeme/baekjoon/2023/january/23968.kt
@@ -0,0 +1,68 @@
+package com.potatomeme.baekjoon.`2023`.january
+
+//https://www.acmicpc.net/problem/23968
+
+//알고리즘 수업 - 버블 정렬 1
+//
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//1 초	512 MB	1048	442	365	44.512%
+//문제
+//오늘도 서준이는 버블 정렬 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+//
+//N개의 서로 다른 양의 정수가 저장된 배열 A가 있다. 버블 정렬로 배열 A를 오름차순 정렬할 경우 K 번째 교환되는 수를 구해서 우리 서준이를 도와주자.
+//
+//크기가 N인 배열에 대한 버블 정렬 의사 코드는 다음과 같다.
+//
+//bubble_sort(A[1..N]) { # A[1..N]을 오름차순 정렬한다.
+//    for last <- N downto 2
+//        for i <- 1 to last - 1
+//            if (A[i] > A[i + 1]) then A[i] <-> A[i + 1]  # 원소 교환
+//}
+//입력
+//첫째 줄에 배열 A의 크기 N(5 ≤ N ≤ 10,000), 교환 횟수 K(1 ≤ K ≤ N2)가 주어진다.
+//
+//다음 줄에 서로 다른 배열 A의 원소 A1, A2, ..., AN이 주어진다. (1 ≤ Ai ≤ 109)
+//
+//출력
+//K 번째 교환되는 두 개의 수를 작은 수부터 한 줄에 출력한다. 교환 횟수가 K 보다 작으면 -1을 출력한다.
+//
+//예제 입력 1
+//6 10
+//4 6 5 1 3 2
+//예제 출력 1
+//2 4
+//4 6 5 1 3 2 -> 4 5 6 1 3 2 -> 4 5 1 6 3 2 -> 4 5 1 3 6 2 -> 4 5 1 3 2 6 -> 4 1 5 3 2 6 -> 4 1 3 5 2 6 -> 4 1 3 2 5 6 -> 1 4 3 2 5 6 -> 1 3 4 2 5 6 -> 1 3 2 4 5 6 -> 1 2 3 4 5 6. 총 11회 교환이 발생하고 열 번째 교환은 2와 4이다.
+//
+//예제 입력 2
+//6 12
+//4 6 5 1 3 2
+//예제 출력 2
+//-1
+//교환 횟수 11이 K 보다 작으므로 -1을 출력한다
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n,k) = readLine().split(" ").map { it.toInt() }
+    val arr = IntArray(n)
+    readLine().split(" ").forEachIndexed { index, s ->
+        arr[index] = s.toInt()
+    }
+    print(Solution23968().solution(n,k,arr))
+}
+
+class Solution23968() {
+    fun solution(n:Int,k:Int,arr:IntArray) : String{
+        var count = 0
+        for (i in 0 until n-1){
+            for (j in 1 until n-i){
+                if (arr[j-1] > arr[j]){
+                    count++
+                    if (count == k) return "${arr[j]} ${arr[j-1]}"
+                    val temp = arr[j-1]
+                    arr[j-1] = arr[j]
+                    arr[j] = temp
+                }
+            }
+        }
+        return "-1"
+    }
+}
