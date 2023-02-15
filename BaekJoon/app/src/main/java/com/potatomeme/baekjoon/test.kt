413f0ac9c4bda490d4851fe85d12671574644973
PotatoMeme <kshk0897@gmail.com>
2023-02-15 22:05:13 +0900

    [init] - [android]

--- BaekJoon/app/src/main/java/com/potatomeme/baekjoon/test.kt
+++ BaekJoon/app/src/main/java/com/potatomeme/baekjoon/test.kt
@@ -4,14 +4,35 @@ fun main() = with(System.`in`.bufferedReader()) {
 
 }
 
-fun insertionSort(arr: IntArray) {
-    for (index in 1 until arr.size) {
-        val temp = arr[index]
-        var aux = index - 1
-        while (aux >= 0 && arr[aux] > temp) {
-            arr[aux + 1] = arr[aux]
-            aux--
+fun mergeSort(A: IntArray, low: Int, high: Int, B: IntArray) {
+    // 1. base condition
+    if (low >= high) return
+
+    // 2. divide
+    val mid = (low + high) / 2
+
+    // 3. conquer
+    mergeSort(A, low, mid, B)
+    mergeSort(A, mid + 1, high, B)
+
+    // 4. combine
+    var i = low
+    var j = mid + 1
+
+    for (k in low..high) {
+        if (j > high) {
+            B[k] = A[i++]
+        } else if (i > mid) {
+            B[k] = A[j++]
+        }else if (A[i] <= A[j]) {
+            B[k] = A[i++]
+        }else {
+            B[k] = A[j++]
         }
-        arr[aux + 1] = temp
+    }
+
+    // 5. copy
+    for (i in low..high){
+        A[i] = B[i]
     }
 }
