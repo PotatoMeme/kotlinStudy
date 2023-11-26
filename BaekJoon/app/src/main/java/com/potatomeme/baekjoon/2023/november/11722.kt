d2d3a96dae302bf1eca5f349f03d0e66e16f8a6e
PotatoMeme <kshk0897@gmail.com>
2023-11-26 15:30:13 +0900

    [baekjoon] - [solve] 11722

---
+++ BaekJoon/app/src/main/java/com/potatomeme/baekjoon/2023/november/11722.kt
@@ -0,0 +1,45 @@
+package com.potatomeme.baekjoon.`2023`.november
+
+import java.util.StringTokenizer
+
+//가장 긴 감소하는 부분 수열
+//
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//1 초	256 MB	32115	20001	16399	63.219%
+//문제
+//수열 A가 주어졌을 때, 가장 긴 감소하는 부분 수열을 구하는 프로그램을 작성하시오.
+//
+//예를 들어, 수열 A = {10, 30, 10, 20, 20, 10} 인 경우에 가장 긴 감소하는 부분 수열은 A = {10, 30, 10, 20, 20, 10}  이고, 길이는 3이다.
+//
+//입력
+//첫째 줄에 수열 A의 크기 N (1 ≤ N ≤ 1,000)이 주어진다.
+//
+//둘째 줄에는 수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+//
+//출력
+//첫째 줄에 수열 A의 가장 긴 감소하는 부분 수열의 길이를 출력한다.
+//
+//예제 입력 1
+//6
+//10 30 10 20 20 10
+//예제 출력 1
+//3
+
+fun main() {
+    with(System.`in`.bufferedReader()) {
+        val n = readLine().toInt()
+        val st = StringTokenizer(readLine())
+        val arr = IntArray(n) { st.nextToken().toInt() }
+        val lenArr = IntArray(n) { 1 }
+        var max = 1
+        for (i in 1 until n) {
+            for (j in 0 until i) {
+                if (arr[i] < arr[j] && lenArr[j] + 1 > lenArr[i]) {
+                    lenArr[i] = lenArr[j] + 1
+                }
+            }
+            if (max < lenArr[i]) max = lenArr[i]
+        }
+        print(max)
+    }
+}
