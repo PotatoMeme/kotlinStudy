0f312fb2233957596f5b4e19a9712fbeb3533a1c
PotatoMeme <kshk0897@gmail.com>
2023-12-01 12:04:10 +0900

    [baekjoon] - [solve] 13549

---
+++ BaekJoon/app/src/main/java/com/potatomeme/baekjoon/2023/december/13549.kt
@@ -0,0 +1,59 @@
+package com.potatomeme.baekjoon.`2023`.december
+
+import java.util.LinkedList
+import java.util.Queue
+
+//숨바꼭질 3
+//
+//시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+//2 초	512 MB	90366	22999	15374	24.263%
+//문제
+//수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 수빈이는 걷거나 순간이동을 할 수 있다. 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 0초 후에 2*X의 위치로 이동하게 된다.
+//
+//수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+//
+//입력
+//첫 번째 줄에 수빈이가 있는 위치 N과 동생이 있는 위치 K가 주어진다. N과 K는 정수이다.
+//
+//출력
+//수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
+//
+//예제 입력 1
+//5 17
+//예제 출력 1
+//2
+
+
+fun main() {
+    with(System.`in`.bufferedReader()) {
+        val (n, k) = readLine().split(" ").map { it.toInt() }
+        val visited = BooleanArray(100_002)
+        val q: Queue<Pair<Int, Int>> = LinkedList() // depth,space
+        q.add(Pair(0, n))
+        while (q.isNotEmpty()) {
+            val (depth, index) = q.poll()!!
+            if (index == k) {
+                print(depth)
+                break
+            } else if (visited[index]) {
+                continue
+            } else if (index == 0) {
+                visited[0] = true
+                if (!visited[1]) q.add(Pair(depth + 1, 1))
+            } else {
+                var currentIndex = index
+                while (currentIndex < 100_001) {
+                    if (visited[currentIndex]) break
+                    if (currentIndex == k) {
+                        print(depth)
+                        return
+                    }
+                    visited[currentIndex] = true
+                    if (!visited[currentIndex - 1]) q.add(Pair(depth + 1, currentIndex - 1))
+                    if (!visited[currentIndex + 1]) q.add(Pair(depth + 1, currentIndex + 1))
+                    currentIndex *= 2
+                }
+            }
+        }
+    }
+}
